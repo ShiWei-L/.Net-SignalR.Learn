bdd6f6fa3c3c7830c8822a569830876df6019bd7
梁士伟 <deawback@hotmail.com>
2017-01-26 20:47:44 +0800

    添加代码压缩

--- SignalR/ConsoleSignalRServer/Startup.cs
+++ SignalR/ConsoleSignalRServer/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Owin;
@@ -13,15 +15,15 @@ namespace ConsoleSignalRServer
     {
         public void Configuration(IAppBuilder app)
         {
-            GlobalHost.DependencyResolver.UseRedis("localhost", 6379, string.Empty, "signalR");
-
-            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
-            app.UseCors(CorsOptions.AllowAll).MapSignalR();
+            //GlobalHost.DependencyResolver.UseRedis("localhost", 6379, string.Empty, "signalR");
 
             //SqlServer
             //GlobalHost.DependencyResolver.UseSqlServer("server=localhost ;user id=sa; password=SqlServerByLSW; Database=signalR");
 
-            
+          
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCors(CorsOptions.AllowAll).MapSignalR();
         }
     }
+
 }
--- SignalR/SimpleHub/Startup.cs
+++ SignalR/SimpleHub/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 using Microsoft.Owin;
 using Owin;
 
@@ -14,8 +16,24 @@ namespace SimpleHub
         {
             // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
 
+            //把自己的实现注入到容器中 n
+            GlobalHost.DependencyResolver.Register(typeof(IJavaScriptMinifier), () => new DefaultJavaScriptMinifier());
+
             //也可以  app.MapSignalR();
             app.MapSignalR("/simpleHub", new HubConfiguration());
+        }
+    }
+
+
+
+    /// <summary>
+    /// 自定义 js 压缩
+    /// </summary>
+    public class DefaultJavaScriptMinifier : IJavaScriptMinifier
+    {
+        public string Minify(string source)
+        {
+            return new Minifier().MinifyJavaScript(source);
         }
     }
 }
